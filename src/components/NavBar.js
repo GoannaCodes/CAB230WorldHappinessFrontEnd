d7fcb97c85ca55a987ee8c7c790ceee6f910fa83
Anna Nguyen <anguy100@hotmail.com>
2021-05-03 10:57:14 +1000

    Navbar routing

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Navbar = () =>{
+    return(
+        <header>
+            <nav>
+                {/* main endpoints */}
+                <ul>
+                    <li id="logo">
+                        <Link to="/home"><img src="icons/happy-logo.png" alt="Home-logo"></img></Link>
+                    </li>
+                    <li>]
+                        <Link to="/rankings" className="NavItems">Rankings</Link>
+                    </li>
+                    <li>
+                        <Link to="/factors" className="NavItems">Factors</Link>
+                    </li>
+                </ul>
+
+                {/* Authentication endpoints */}
+                <Link to={"/register"}>
+                    <Button variant="contained" color="primary"id="registerButton">Register</Button>
+                </Link>
+                <Link to={"/login"}>
+                    <Button variant="contained" id="loginButton">Login</Button>
+                </Link>
+                <Link to={"/logout"}>
+                    <Button variant="contained" color="secondary" id="logoutButton">Logout</Button>
+                </Link>
+            </nav>
+        </header>
+    )
+}
