82ad1765b5c8d156539ce4dd46f2f7e806e710a9
Anna Nguyen <anguy100@hotmail.com>
2021-05-03 13:08:58 +1000

    Implemented register page

---
+++ src/components/Register.js
@@ -0,0 +1,82 @@
+import React, {useState} from "react";
+import {Grid, Paper, Button, TextField} from "@material-ui/core";
+import {Link} from "react-router-dom";
+
+export const paperStyle={
+    padding: "20px 30px",
+    width: "300px",
+    margin: "30px auto"
+}
+
+function Email(props){
+    const[innerEmail, setInnerEmail] = useState("");
+    return(
+        <TextField 
+            label="Email"
+            helperText="Enter email"
+            type="email"
+            value={innerEmail}
+            onChange={event=>{
+                const {value} = event.target;
+                setInnerEmail(value)
+                props.onChange(value);
+            }}/>
+    )
+}
+
+function Password(props){
+    const[innerPassword, setInnerPassword] = useState("");
+    return(
+        <TextField 
+            label="Password"
+            helperText="Enter password"
+            type="password"
+            value={innerPassword}
+            onChange={event=>{
+                const {value} = event.target;
+                setInnerPassword(value)
+                props.onChange(value);
+            }}/>
+    )
+}
+
+export function Register(){
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const handleSubmit = event =>{
+        event.preventDefault();
+
+        fetch("http://131.181.190.87:3000/user/register", {
+            method:"POST",
+            headers:{
+                "Content-type":"application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({email: `${email}`, password: `${pass}`})
+        })
+        .then(res=> res.json())
+        .then(res => console.log(res))
+        .catch(()=> alert("There was an error, please try again"))
+    }
+    return(
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                    <h1 className="form-title">Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <Email onChange={setEmail}/>
+                        <Password onChange={setPass}/>
+                        <Button
+                        variant="contained"
+                        type="submit"
+                        style={{marginTop: "15px" }}
+                        color="primary">Create an account</Button>
+                    </form>
+                </Grid>
+            </Paper>
+            <Paper elevation={20} style={paperStyle} >
+                <p>Already have an account? <Link to="/login">Login here!</Link></p>
+           </Paper>
+        </Grid>
+    )
+}
