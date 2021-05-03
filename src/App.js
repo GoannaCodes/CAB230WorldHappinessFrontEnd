6b0e1ebe3c8d34ce662631fc568cddad75083abc
Anna Nguyen <anguy100@hotmail.com>
2021-05-03 11:11:14 +1000

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Switch, BrowserRouter as Router, Route} from "react-router-dom";
+
+import Home from "./pages/Home";
+import {Records} from "./pages/Records";
+import Factors from "./pages/Factors";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/rankings" component={Records}/>
+          <Route exact path="/factors" component={Factors}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/logout" component={Logout}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
