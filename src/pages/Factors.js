c03b35d4e392fe3b2bb73e8e6e48c7c11cba173b
Anna Nguyen <anguy100@hotmail.com>
2021-05-05 22:43:39 +1000

    Imported country & year selection to factors from records

--- src/pages/Factors.js
+++ src/pages/Factors.js
@@ -1,21 +1,25 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import {CountrySelection, YearSelection} from "./Records";
 
 const token = localStorage.getItem("token");
 
 function AuthenticatedContent(props){
-    const[selectedYear, setSelectedCountry] = useState("2015");
+    const[selectedYear, setSelectedYear] = useState("2015");
+    const [selectedCountry, setSelectedCountry] = useState("");
     return(
-        <div>
+        <div className="content">
             <h1>Factors</h1>
             <p>You have access to this content</p>
+            <YearSelection onInputChange={setSelectedYear} />
+            <CountrySelection onInputChange={setSelectedCountry}/>
         </div>
     )
 }
 
 function UnauthenticatedContent(props){
     return(
-        <div>
+        <div className="content">
             <h1>Factors</h1>
             <p>You need to <Link to="/login">login</Link> to access this content</p>
         </div>
