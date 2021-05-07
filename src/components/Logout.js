4491ef4b47f8e4c7efd8e5b4f4f87d28fb235714
Anna Nguyen <anguy100@hotmail.com>
2021-05-07 11:35:43 +1000

    Logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,13 +3,13 @@ export default function Logout(){
     if ("token" in localStorage && token !== "undefined"){
         localStorage.removeItem("token");
         token="";
-        return(
-            <div className="content">
-                <h1>Logout</h1>
-                <p>You have successfully logged out.</p>
-            </div>
-        )
     } 
+    return(
+        <div className="content">
+            <h1>Logout</h1>
+            <p>You have been logged out.</p>
+        </div>
+    )
     // if token expires logout here too
     
 }
